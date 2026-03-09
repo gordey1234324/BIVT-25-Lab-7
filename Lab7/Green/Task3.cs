2eaa000b8fb854385b00a3ce468ba4927fcaf531
seyrinf <gub-sava@yandex.ru>
2026-03-09 22:52:45 +0300

    Update Task3.cs

--- Lab7/Green/Task3.cs
+++ Lab7/Green/Task3.cs
@@ -1,8 +1,93 @@
-﻿using System.Collections;
+
+using System.Collections;
 
 namespace Lab7.Green
 {
     public class Task3
     {
+        public struct Student
+        {
+            private string _name;
+            private string _surname;
+            private int[] _marks;
+            private bool _sost = false;
+
+            public string Name => _name;
+            public string Surname => _surname;
+            public int[] Marks => _marks.ToArray();
+            public bool IsExpelled => _sost;
+
+            public double AverageMark
+            {
+                get
+                {
+                    int validMarksCount = 0;
+                    double sum = 0;
+
+                    for (int i = 0; i < _marks.Length; i++)
+                    {
+                        if (_marks[i] > 0)
+                        {
+                            sum += _marks[i];
+                            validMarksCount++;
+                        }
+                    }
+
+                    if (validMarksCount == 0) return 0;
+
+                    return sum / validMarksCount;
+                }
+            }
+
+            public Student(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _marks = new int[3];
+            }
+
+            public void Exam(int mark)
+            {
+                if (_sost) return;
+
+                for (int i = 0; i < _marks.Length; i++)
+                {
+                    if (_marks[i] == 0)
+                    {
+                        if (mark > 2)
+                        {
+                            _marks[i] = mark;
+                            break;
+                        }
+
+                        if (mark == 2)
+                        {
+                            _sost = true;
+                            _marks[i] = mark;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            public static void SortByAverageMark(Student[] array)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    for (int j = 0; j < array.Length - i - 1; j++)
+                    {
+                        if (array[j].AverageMark < array[j + 1].AverageMark)
+                        {
+                            (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                        }
+                    }
+                }
+            }
+
+            public void Print()
+            {
+                return;
+            }
+        }
     }
 }
