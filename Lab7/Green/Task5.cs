7248ce5dffba242b7f1b0c65d302b92939eab5cf
seyrinf <gub-sava@yandex.ru>
2026-03-09 22:53:19 +0300

    Update Task5.cs

--- Lab7/Green/Task5.cs
+++ Lab7/Green/Task5.cs
@@ -1,6 +1,90 @@
-﻿namespace Lab7.Green
+namespace Lab7.Green
 {
     public class Task5
     {
+        public struct Student
+        {
+            private string _name;
+            private string _surname;
+            private int[] _marks;
+
+            public string Name => _name;
+            public string Surname => _surname;
+            public int[] Marks => _marks.ToArray();
+            public double AverageMark => _marks.Average();
+
+            public Student(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _marks = new int[5];
+            }
+
+            public void Exam(int mark)
+            {
+                for (int i = 0; i < _marks.Length; i++)
+                {
+                    if (_marks[i] == 0)
+                    {
+                        _marks[i] = mark;
+                        break;
+                    }
+                }
+            }
+
+            public void Print()
+            {
+                return;
+            }
+        }
+
+        public struct Group
+        {
+            private string _name;
+            private Student[] _students;
+
+            public string Name => _name;
+            public Student[] Students => _students.ToArray();
+            public double AverageMark => _students.Average(x => x.AverageMark);
+
+            public Group(string name)
+            {
+                _name = name;
+                _students = new Student[0];
+            }
+
+            public void Add(Student student)
+            {
+                Array.Resize(ref _students, _students.Length + 1);
+                _students[_students.Length - 1] = student;
+            }
+
+            public void Add(Student[] students)
+            {
+                int old = _students.Length;
+                Array.Resize(ref _students, old + students.Length);
+
+                for (int i = 0; i < students.Length; i++)
+                    _students[old + i] = students[i];
+            }
+            public static void SortByAverageMark(Group[] array)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    for (int j = 0; j < array.Length - i - 1; j++)
+                    {
+                        if (array[j].AverageMark < array[j + 1].AverageMark)
+                        {
+                            (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                        }
+                    }
+                }
+            }
+
+            public void Print()
+            {
+                return;
+            }
+        }
     }
 }
